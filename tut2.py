25deaf2fa4a62e826c5204da30750f881e4bc41a
Piyanshi12 <piyanshijain12@gmail.com>
2026-07-12 15:38:48 +0530

    Add files via upload

---
+++ tut2.py
@@ -0,0 +1,15 @@
+from tempfile import template
+
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+
+
+@app.route('/about')
+def piyanshi():
+    return render_template('about.html')
+app.run(debug = True)
